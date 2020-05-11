f3afed196b374555c6a88f2f690dc54eb7f9bfe6
Ben Buhse <ben@buhses.com>
2020-05-11 16:43:54 -0500

    Update main to work even when no arguments are passed

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,12 @@ use std::env;
 fn main() {
     let mut args = env::args().collect::<Vec<String>>();
 
-    match args.swap_remove(1).as_ref() {
-        "sock_merchant" => sock_merchant::run(),
-        _ => println!("What'd you do"),
-    };
+    if args.len() > 1 {
+        match args.swap_remove(1).as_ref() {
+            "sock_merchant" => sock_merchant::run(),
+            _ => println!("Invalid program name"),
+        };
+    } else {
+        println!("You need to pass program that you want to run as an arg");
+    }
 }
