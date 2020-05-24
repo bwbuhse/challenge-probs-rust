810082cbd98e2500150b5611087c31fad23d3914
Ben Buhse <ben@buhses.com>
2020-05-24 01:11:59 -0500

    Get Code Wars's Tribonacci working

--- src/code_wars/tribonacci.rs
+++ src/code_wars/tribonacci.rs
@@ -4,9 +4,19 @@ pub fn seq(signature: &[f64; 3], n: usize) -> Vec<f64> {
     sig_sorted.clone_from_slice(&signature[0..]);
 
     if n > 0 && *signature == sig_sorted {
-        for i in 3..n {
-            vec.push(vec[i - 3] + vec[i - 2] + vec[i - 1]);
+        // These two are special cases
+        if n == 1 {
+            vec.pop();
+            vec.pop();
+        } else if n == 2 {
+            vec.pop();
+        } else {
+            for i in 3..n {
+                vec.push(vec[i - 3] + vec[i - 2] + vec[i - 1]);
+            }
         }
+    } else {
+        vec.clear();
     }
 
     vec
