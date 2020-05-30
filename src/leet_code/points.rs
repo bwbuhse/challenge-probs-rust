be3de771a30f5eb72ebac66434cfb12c4224be18
Ben Buhse <ben@buhses.com>
2020-05-30 11:01:41 -0500

    Make Leet Code Points slightly more efficient

--- src/leet_code/points.rs
+++ src/leet_code/points.rs
@@ -1,3 +1,4 @@
+// https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/538/week-5-may-29th-may-31st/3345/
 pub fn k_closest(points: Vec<Vec<i32>>, k: i32) -> Vec<Vec<i32>> {
     let mut sorted_points = points.clone();
     sorted_points.sort_by(|a, b| {
@@ -6,10 +7,6 @@ pub fn k_closest(points: Vec<Vec<i32>>, k: i32) -> Vec<Vec<i32>> {
             .partial_cmp(&((b[0] * b[0] + b[1] * b[1]) as f64).sqrt())
             .unwrap()
     });
-    let mut k_closest = Vec::new();
 
-    for i in 0..k {
-        k_closest.push(sorted_points[i as usize].clone());
-    }
-    k_closest
+    sorted_points[0..k as usize].to_vec()
 }
